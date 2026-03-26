d341b469e6db78dfe5780c47fd04ce3bd53c3816
karunamin <freedom12528@gmail.com>
2026-03-26 17:27:30 +0900

    커밋

---
+++ day01/ex03_logic_control.py
@@ -0,0 +1,23 @@
+# ex03_logic_control 분기문/반복문
+
+age = int(input('나이는? '))
+
+if age <= 19:
+    print('집에 가세요.')
+elif age > 30 and age <= 60:   # else if
+    print('못들어가세요.')
+else:
+    print("어, 들어가세요~")
+    print('어서 오세요!!')
+
+
+# 반복문
+num = int(input('반복횟수 > '))
+
+for i in range(num):
+    print(i)
+
+print('---')
+
+for i in range(1, num + 1): # 1 ~ 4
+    print(i)
